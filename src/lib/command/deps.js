99039cec536a7e8c7a3dc9f9b77c6fd8a5b90d14
Leo Brad <>
2024-03-10 11:43:03 +0800

    [Add] Add related deps.

--- src/lib/command/deps.js
+++ src/lib/command/deps.js
@@ -2,12 +2,16 @@ import { execSync, } from 'child_process';
 import chalk from 'chalk';
 import askQuestion from '~/lib/util/askQuestion';
 
-async function addDeps(deps) {
+async function addDeps(deps, dev) {
   const result = await askQuestion(
     'Are you sure install ' + chalk.yellow('"' + deps + '"') + ' in current project.'
   );
   if (result === true) {
-    execSync('yarn add --dev ' + deps, { stdio: 'inherit', });
+    if (dev === true) {
+      execSync('yarn add --dev ' + deps, { stdio: 'inherit', });
+    } else {
+      execSync('yarn add ' + deps, { stdio: 'inherit', });
+    }
     console.log([
       chalk.bold('The ' + chalk.green('"' + deps + '"') + ' dependence had be installed.'),
       '',
@@ -16,23 +20,28 @@ async function addDeps(deps) {
 }
 
 export default async function deps(...param) {
-  await addDeps('@babel/cli');
-  await addDeps('@babel/core');
-  await addDeps('@babel/preset-env');
-  await addDeps('@babel/preset-react');
-  await addDeps('@babel/register');
-  await addDeps('autoprefixer');
-  await addDeps('babel-loader');
-  await addDeps('babel-plugin-root-import');
-  await addDeps('css-loader');
-  await addDeps('file-loader');
-  await addDeps('gulp-babel');
-  await addDeps('gulp');
-  await addDeps('html-webpack-plugin');
-  await addDeps('postcss-loader');
-  await addDeps('style-loader');
-  await addDeps('sugarss');
-  await addDeps('webpack');
-  await addDeps('webpack-cli');
-  await addDeps('webpack-dev-server');
+  await addDeps('@babel/cli', true);
+  await addDeps('@babel/core', true);
+  await addDeps('@babel/preset-env', true);
+  await addDeps('@babel/preset-react', true);
+  await addDeps('@babel/register', true);
+  await addDeps('autoprefixer', true);
+  await addDeps('babel-loader', true);
+  await addDeps('babel-plugin-root-import', true);
+  await addDeps('css-loader', true);
+  await addDeps('file-loader', true);
+  await addDeps('gulp-babel', true);
+  await addDeps('html-webpack-plugin', true);
+  await addDeps('postcss-loader', true);
+  await addDeps('style-loader', true);
+  await addDeps('sugarss', true);
+  await addDeps('webpack', true);
+  await addDeps('webpack-cli', true);
+  await addDeps('webpack-dev-server', true);
+
+  await addDeps('react', false);
+  await addDeps('react-dom', false);
+  await addDeps('gulp', false);
+  await addDeps('glow.js', false);
+  await addDeps('manner.js', false);
 }
