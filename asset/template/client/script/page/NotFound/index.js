822c11d007510cffea9f39075ae871ad89d3345c
Leo Ely <leoely789@gmail.com>
2026-07-28 16:15:12 +0800

    [Redesign] Redesigned page not found.

--- asset/template/client/script/page/NotFound/index.js
+++ asset/template/client/script/page/NotFound/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import {
-  FontAwesomeIcon,
-} from '@fortawesome/react-fontawesome';
-import {
-  faEarthAmericas,
-} from '@fortawesome/free-solid-svg-icons';
 import Page from '~/client/script/component/Page';
+import Image from '~/client/script/component/Image';
+import notFoundImg from './not-found.png';
 import formatLocation from '~/client/script/lib/util/formatLocation';
 import * as style from './index.module.css';
 
 class NotFound extends Page {
   render() {
     return (
-      <div className={style.notfound}>
-        <FontAwesomeIcon className={style.earthIcon} icon={faEarthAmericas} />
-        <span className={style.question}>?</span>
-        Location
-        <span className={style.location}>{formatLocation(location.pathname)}</span>
-        don't exist.
+      <div className={style.notFound}>
+        <div className={style.detail}>
+          <h2 className={style.title}>404 [Not Found]</h2>
+          <p className={style.passage}>
+            No corresponding resource found at the current location.
+            Please confirm that the current location is correct.
+          </p>
+          <div className={style.explain}>
+            Location
+            <span className={style.location}>{formatLocation(location.pathname)}</span>
+            don't exist.
+          </div>
+        </div>
+        <Image className={style.image} src={notFoundImg} />
       </div>
     );
   }
